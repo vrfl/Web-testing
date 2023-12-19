3148aa9dadb44357731f8afdd342a8bd73d3ab1e
vrfl <17ametista17@gmail.com>
2023-12-19 16:32:38 +0700

    Выполнено задание

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -53,4 +53,66 @@ public class MainClass {
 
         driver.quit();
     }
+
+    @Test
+    public void testForMail() {
+        System.setProperty("webdriver.chrome.driver", "/Users/wachanga/Documents/Web-testing/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        // Откроем сайт mail.ru
+        driver.get("https://mail.ru");
+        driver.manage().window().setSize(new Dimension(1280, 1025));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Нажмем кнопку Войти
+        driver.findElement(
+                By.xpath("//button[@class='resplash-btn resplash-btn_primary resplash-btn_mailbox-big ihfknge-de8k2m']")
+        ).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //  Переключимся на фрейм с авторизцией
+        WebElement frame = driver.findElement(By.xpath("//*[@class='ag-popup__frame__layout__iframe']"));
+        driver.switchTo().frame(frame);
+
+        // Введем логин
+        WebElement usernameInput = driver.findElement(By.xpath("//input[@name='username']"));
+        Assert.assertEquals(
+                usernameInput,
+                driver.switchTo().activeElement()
+        );
+        usernameInput.sendKeys("mega_professional_tester");
+
+        driver.findElement(
+                By.xpath("//button[@data-test-id='next-button']")
+        ).click();
+
+        // Введем пароль
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@name='password']"));
+        passwordInput.sendKeys("nogduV-xedpe4-wekcez");
+
+        // Войдем в аккаунт
+        driver.findElement(By.xpath("//button[@data-test-id='submit-button']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Откроем профиль
+        driver.findElement(By.xpath("//*[@class='ph-avatar-img svelte-dfhuqc']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Проверим что имя в профиле совпадает
+        Assert.assertEquals(
+                "Тестер Супер-дупер",
+                driver.findElement(By.xpath("//*[@class='ph-name svelte-1popff4']")).getText()
+        );
+
+        // Выйдем из аккаунта
+        driver.findElement(By.xpath("//*[@data-testid='whiteline-account-exit']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Удостоверимся, что на экране есть кнопка Создать почту
+        driver.findElement(
+                By.xpath("//a[@class='resplash-btn resplash-btn_outlined-themed resplash-btn_mailbox-big ihfknge-de8k2m']")
+        ).isDisplayed();
+
+        driver.quit();
+    }
 }
